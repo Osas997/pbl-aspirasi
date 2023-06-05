0786f91f5ca88495a9b0cc1290c4bf932584999d
Osas997 <larasatyt83@gmail.com>
2023-06-06 01:29:59 +0700

    tes

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
    <link rel="stylesheet" href="css/style.css">
    <link rel="shortcut icon" href="img/LOGO HMJTI2.png" type="image/x-icon">
+   <script src="https://unpkg.com/feather-icons"></script>
    <title>Website Aspirasi Mahasiswa</title>
 </head>
 
@@ -19,7 +20,7 @@
             <div class="logo-img">
                <img class="img-poli" src="img/LOGO HMJTI2.png" alt="">
             </div>
-            <h3 class="fs-3 fw-bolder mt-2 text-light">POLIWANGI</h3>
+            <h3 class="fs-3 poli fs-sm-1 fw-bolder mt-2 text-light">POLIWANGI</h3>
          </div>
          <div class="link">
             <ul>
@@ -35,6 +36,9 @@
                   </svg><a href="petugas/login_petugas.php">Login Admin / Petugas</a></li>
             </ul>
          </div>
+         <li class="hamburg" id="hamburger-menu">
+            <a><i class="strip" data-feather="menu"></i></a>
+         </li>
       </div>
    </nav>
    <!-- end navbar -->
@@ -49,10 +53,14 @@
                         <div class="teks text-light">
                            <h1>WEB PELAPORAN</h1>
                            <h1>PENGADUAN MAHASISWA</h1>
-                           <h3>Selamat Datang Di Web Pengaduan Mahasiswa Politeknik Negeri Banyuwangi</h3>
-                           <h3>Silahkan Registrasi Atau Login dengan Akun Anda</h3>
-                           <button class="btn-tod" onclick="location.href = 'halaman_mhs.php'">Ayo
-                              Lapor!!</button>
+                           <div class="kata">
+                              <h3 class="text-hero text-center text-md-start">Selamat Datang Di Web Pengaduan Mahasiswa Politeknik Negeri Banyuwangi</h3>
+                              <h3 class="text-hero text-center text-md-start">Silahkan Registrasi Atau Login dengan Akun Anda</h3>
+                           </div>
+                           <div class="btnn">
+                              <button class="btn-tod" onclick="location.href = 'halaman_mhs.php'">Ayo
+                                 Lapor!!</button>
+                           </div>
                         </div>
                      </div>
                   </div>
@@ -67,10 +75,14 @@
                         <div class="teks text-light">
                            <h1>WEB PELAPORAN</h1>
                            <h1>PENGADUAN MAHASISWA</h1>
-                           <h3>Selamat Datang Di Web Pengaduan Mahasiswa Politeknik Negeri Banyuwangi</h3>
-                           <h3>Silahkan Registrasi Atau Login dengan Akun Anda</h3>
-                           <button class="btn-tod" onclick="location.href = 'halaman_mhs.php'">Ayo
-                              Lapor!!</button>
+                           <div class="kata">
+                              <h3 class="text-hero text-center text-md-start">Selamat Datang Di Web Pengaduan Mahasiswa Politeknik Negeri Banyuwangi</h3>
+                              <h3 class="text-hero text-center text-md-start">Silahkan Registrasi Atau Login dengan Akun Anda</h3>
+                           </div>
+                           <div class="btnn">
+                              <button class="btn-tod" onclick="location.href = 'halaman_mhs.php'">Ayo
+                                 Lapor!!</button>
+                           </div>
                         </div>
                      </div>
                   </div>
@@ -139,6 +151,9 @@
 
 
    <script src="js/script.js"></script>
+   <script>
+      feather.replace()
+   </script>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
    </script>
 </body>
